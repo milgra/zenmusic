59b422a457634bce871e626a0c669dcb277400ed
Milan Toth <milgra@milgra.com>
2020-11-22 18:22:48 +0100

    knob antialiasing added

--- src/eh_knob.c
+++ src/eh_knob.c
@@ -15,6 +15,8 @@ void eh_knob_add(view_t* view);
 typedef struct _eh_knob_t
 {
   char* id;
+  bm_t* top;
+  bm_t* bottom;
 } eh_knob_t;
 
 void eh_knob_evt(view_t* view, ev_t ev)
@@ -23,11 +25,27 @@ void eh_knob_evt(view_t* view, ev_t ev)
   {
     if (view->texture.bitmap == NULL && view->frame.local.w > 0 && view->frame.local.h > 0)
     {
-      bm_t* bmp = bm_new(view->frame.local.w, view->frame.local.h);
+      eh_knob_t* eh = view->evt_han_data;
 
-      mtgraphics_circle(bmp, (view->frame.local.w - 1.0) / 2.0, (view->frame.local.h - 1.0) / 2.0, (view->frame.local.w / 2.0 - 2.0), 1.0, 0x999999FF);
-      mtgraphics_circle(bmp, (view->frame.local.w - 1.0) / 2.0, (view->frame.local.h - 1.0) / 2.0, 30.0, 1.0, 0x666666FF);
+      bm_t* bmp  = bm_new(view->frame.local.w, view->frame.local.h);
+      eh->top    = bm_new(view->frame.local.w, view->frame.local.h);
+      eh->bottom = bm_new(view->frame.local.w, view->frame.local.h);
 
+      mtgraphics_circle(eh->bottom,
+                        (view->frame.local.w - 1.0) / 2.0,
+                        (view->frame.local.h - 1.0) / 2.0,
+                        (view->frame.local.w / 2.0 - 2.0),
+                        1.0,
+                        0x999999FF);
+      mtgraphics_circle(eh->top,
+                        (view->frame.local.w - 1.0) / 2.0,
+                        (view->frame.local.h - 1.0) / 2.0,
+                        30.0,
+                        1.0,
+                        0x666666FF);
+
+      bm_insert(bmp, eh->bottom, 0, 0);
+      bm_insert_blend(bmp, eh->top, 0, 0);
       view_set_texture_bmp(view, bmp);
     }
   }
@@ -39,14 +57,15 @@ void eh_knob_evt(view_t* view, ev_t ev)
         ev.y > view->frame.global.y &&
         view->texture.bitmap)
     {
+      eh_knob_t* eh = view->evt_han_data;
+
       float dx    = ev.x - (view->frame.global.x + view->frame.global.w / 2.0);
       float dy    = ev.y - (view->frame.global.y + view->frame.global.h / 2.0);
       float angle = atan2(dy, dx);
 
       if (angle < 0) angle += 6.28;
 
-      printf("angle %f\n", angle * 180 / 3.14);
-      mtgraphics_circle(view->texture.bitmap, (view->frame.local.w - 1.0) / 2.0, (view->frame.local.h - 1.0) / 2.0, (view->frame.local.w / 2.0 - 2.0), 1.0, 0x999999FF);
+      bm_insert(view->texture.bitmap, eh->bottom, 0, 0);
 
       if (angle > 3.14 * 3 / 2)
       {
@@ -79,16 +98,9 @@ void eh_knob_evt(view_t* view, ev_t ev)
                        angle);
       }
 
-      mtgraphics_circle(view->texture.bitmap,
-                        (view->frame.local.w - 1.0) / 2.0,
-                        (view->frame.local.h - 1.0) / 2.0,
-                        30.0,
-                        2.0,
-                        0x666666FF);
+      bm_insert_blend(view->texture.bitmap, eh->top, 0, 0);
 
       view->texture.changed = 1;
-
-      eh_knob_t* eh = view->evt_han_data;
     }
   }
 }
