0ecd06ea9d068162fe88d404b6f785ddeffe4d7b
Milan Toth <milgra@milgra.com>
2020-12-17 16:45:02 +0100

    text rendering refactor

--- src/modules/text/text.c
+++ src/modules/text/text.c
@@ -53,6 +53,9 @@ typedef struct _glyph_t
   float    y_scale;
   float    x_shift;
   float    y_shift;
+  float    asc;
+  float    desc;
+  float    base_y;
   uint32_t cp;
 } glyph_t;
 
@@ -157,8 +160,8 @@ void text_break_glyphs(
     int ncp = index < count - 1 ? glyphs[index + 1].cp : 0;
 
     int   x0, y0, x1, y1;
-    float x_shift = xpos - (float)floor(xpos); // subpixel shift
-    float y_shift = ypos - (float)floor(ypos);
+    float x_shift = xpos - (float)floor(xpos); // subpixel shift along x
+    float y_shift = ypos - (float)floor(ypos); // subpixel shift along y
 
     stbtt_GetCodepointHMetrics(font,
                                cp,
@@ -206,6 +209,9 @@ void text_break_glyphs(
       glyph.y_scale = scale;
       glyph.x_shift = x_shift;
       glyph.y_shift = y_shift;
+      glyph.base_y  = ypos;
+      glyph.asc     = (float)asc * scale;
+      glyph.desc    = (float)desc * scale;
       glyph.cp      = cp;
 
       // printf("write glyph %c at xpos %f w %i h %i basey(y) %i (x) %i x_shift %f\n", text[ch], xpos, x1 - x0, y1 - y0, basey, (int)xpos + x0, x_shift);
@@ -242,43 +248,52 @@ void text_align_glyphs(glyph_t*    glyphs,
                        int         w,
                        int         h)
 {
-  // calculate vertical shift
-  float vs = 0; // vertical shift
-
-  for (int i = 0; i < count; i++)
+  if (count > 0)
   {
-    glyph_t g = glyphs[i];
-    float   x = g.x;
-    float   y = g.y;
-    // get last glyph in row for row width
-    float ex = x; // end x
-    float rw = 0; // row width
-    int   ri;     // row index
-    int   sc;     // space count
-    for (ri = i; ri < count; ri++)
+    // calculate vertical shift
+    glyph_t head   = glyphs[0];
+    glyph_t tail   = glyphs[count - 1];
+    float   height = (tail.base_y - tail.desc) - (head.base_y - head.asc);
+    float   vs     = 1.0; // vertical shift
+
+    if (style.valign == VA_CENTER) vs = (h - height) / 2.0;
+    if (style.valign == VA_BOTTOM) vs = h - height;
+
+    for (int i = 0; i < count; i++)
     {
-      glyph_t rg = glyphs[ri];
-      if (rg.y != y)
+      glyph_t g = glyphs[i];
+      float   x = g.x;
+      float   y = g.y;
+      // get last glyph in row for row width
+      float ex = x; // end x
+      float rw = 0; // row width
+      int   ri;     // row index
+      int   sc;     // space count
+      for (ri = i; ri < count; ri++)
       {
-        rw = ex - x;
-        break;
+        glyph_t rg = glyphs[ri];
+        if (rg.y != y)
+        {
+          rw = ex - x;
+          break;
+        }
+        ex = rg.x + rg.w;
+        if (rg.cp == ' ') sc += 1; // count spaces
       }
-      ex = rg.x + rg.w;
-      if (rg.cp == ' ') sc += 1; // count spaces
+      // calculate horizontal shift
+      float hs = 0; // space
+      if (style.align == TA_RIGHT) hs = (float)w - rw;
+      if (style.align == TA_CENTER) hs = ((float)w - rw) / 2.0; // space
+      if (style.align == TA_JUSTIFY) hs = ((float)w - rw) / sc; // space
+      // shift glyphs in both direction
+      for (int ni = i; ni < ri; ni++)
+      {
+        glyphs[ni].x += (int)hs;
+        glyphs[ni].y += (int)vs;
+      }
+      // jump to next row
+      i = ri - 1;
     }
-    // calculate horizontal shift
-    float hs = 0; // space
-    if (style.align == TA_RIGHT) hs = (float)w - rw;
-    if (style.align == TA_CENTER) hs = ((float)w - rw) / 2.0; // space
-    if (style.align == TA_JUSTIFY) hs = ((float)w - rw) / sc; // space
-    // shift glyphs in both direction
-    for (int ni = i; ni < ri; ni++)
-    {
-      glyphs[ni].x += (int)hs;
-      glyphs[ni].y += (int)hs;
-    }
-    // jump to next row
-    i = ri;
   }
 }
 
@@ -355,117 +370,8 @@ void text_render(
   for (int i = 0; i < text->length; i++) glyphs[i].cp = text->codepoints[i];
 
   text_break_glyphs(glyphs, text->length, style, bitmap->w, bitmap->h);
-
-  //  text_align_glyphs(glyphs, text->length, style, bitmap->w, bitmap->h);
-
+  text_align_glyphs(glyphs, text->length, style, bitmap->w, bitmap->h);
   text_render_glyphs(glyphs, text->length, style, bitmap);
-}
-
-void text_render_a(
-    str_t*      text,
-    textstyle_t style,
-    bm_t*       bitmap)
-{
-
-  gfx_rect(bitmap, 0, 0, bitmap->w, bitmap->h, style.backcolor, 0);
-
-  stbtt_fontinfo* font = MGET(fonts, style.font);
-  if (font == NULL)
-  {
-    text_font_load(style.font);
-    font = MGET(fonts, style.font);
-    if (!font) return;
-  }
-
-  int   ascent, descent, linegap, advancey, baseline, baseheight;
-  float scale, xpos = 0; // leave a little padding in case the character extends left
-
-  scale = stbtt_ScaleForPixelHeight(font, style.size);
-  stbtt_GetFontVMetrics(font, &ascent, &descent, &linegap);
-  baseheight = ascent - descent;
-
-  baseline = (int)(ascent * scale);
-
-  if (style.valign == VA_CENTER) baseline += (bitmap->h - (scale * baseheight)) / 2;
-
-  advancey = ascent - descent + linegap;
-
-  // printf("rendering text %s scale %f ascent %i descent %i linegap %i baseline %i advancey %i\n", text, scale, ascent, descent, linegap, baseline, advancey);
-
-  for (int index = 0; index < text->length; index++)
-  {
-    int cp  = text->codepoints[index];
-    int ncp = index < text->length - 1 ? text->codepoints[index + 1] : 0;
-
-    int   lsb;      // left side bearing, from current pos to the left edge of the glyph
-    int   advancex; // advance width from current pos to next pos
-    int   x0, y0, x1, y1;
-    float x_shift = xpos - (float)floor(xpos); // subpixel shift
-
-    stbtt_GetCodepointHMetrics(font,
-                               cp,
-                               &advancex, // advance from base x pos to next base pos
-                               &lsb);     // left side bearing
-
-    // increase xpos with left side bearing if first character in line
-    if (xpos == 0 && lsb < 0)
-    {
-      xpos    = (float)-lsb * scale;
-      x_shift = xpos - (float)floor(xpos); // subpixel shift
-    }
-
-    // printf("codepoint h metrics %c advance %i left side bearing %i\n", cp, advancex, lsb);
-
-    stbtt_GetCodepointBitmapBoxSubpixel(font,
-                                        cp,
-                                        scale,
-                                        scale,
-                                        x_shift, // x axis subpixel shift
-                                        0,       // y axis subpixel shift
-                                        &x0,     // left edge of the glyph from origin
-                                        &y0,     // top edge of the glyph from origin
-                                        &x1,     // right edge of the glyph from origin
-                                        &y1);    // bottom edge of the glyph from origin
-
-    // printf("bitmap subpixel %c x0 %i y0 %i x1 %i y1 %i\n", cp, x0, y0, x1, y1);
-
-    int w = x1 - x0;
-    int h = y1 - y0;
-
-    int size = w * h;
-
-    // increase glyph baking bitmap size if needed
-    if (size > gcount)
-    {
-      gcount = size;
-      gbytes = realloc(gbytes, gcount);
-    }
-
-    // don't write bitmap in case of empty glyphs ( space )
-    if (w > 0 && h > 0)
-    {
-      stbtt_MakeCodepointBitmapSubpixel(font,
-                                        gbytes,
-                                        w,       // out widht
-                                        h,       // out height
-                                        w,       // out stride
-                                        scale,   // scale x
-                                        scale,   // scale y
-                                        x_shift, // shift x
-                                        0,       // shift y
-                                        cp);
-
-      gfx_blend_8(bitmap, xpos + x0, baseline + y0, style.textcolor, gbytes, w, h);
-
-      // printf("write glyph %c at xpos %f w %i h %i baseline(y) %i (x) %i x_shift %f\n", text[ch], xpos, x1 - x0, y1 - y0, baseline, (int)xpos + x0, x_shift);
-    }
-
-    // advance x axis
-    xpos += (advancex * scale);
-
-    // advance with kerning
-    if (ncp > 0) xpos += scale * stbtt_GetCodepointKernAdvance(font, cp, ncp);
-  }
 }
 
 #endif
