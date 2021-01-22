cf3804d355d79576944f64a6bc98218a34f32212
Milan Toth <milgra@milgra.com>
2021-01-22 08:35:13 +0100

    overflow property added

--- src/modules/view/view_generator.c
+++ src/modules/view/view_generator.c
@@ -88,7 +88,10 @@ void view_gen_apply_style(view_t* view, map_t* style, char* respath)
         view->layout.display = LD_FLEX;
       if (strcmp(val, "none") == 0)
         view->display = 0;
-      if (strcmp(val, "mask") == 0)
+    }
+    else if (strcmp(key, "overflow") == 0)
+    {
+      if (strcmp(val, "hidden") == 0)
         view->masked = 1;
     }
     else if (strcmp(key, "flex-direction") == 0)
