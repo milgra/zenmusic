808902ea46ad8cf038f440afb8267d20cf252b82
Milan Toth <milgra@milgra.com>
2021-03-31 16:21:43 +0200

    refactor

--- src/modules/view/vh_textinput.c
+++ src/modules/view/vh_textinput.c
@@ -8,11 +8,12 @@
 
 typedef struct _vh_textinput_t
 {
-  str_t*      text;
-  vec_t*      glyph_views;
-  view_t*     cursor;
+  str_t*  text_s;
+  vec_t*  glyph_v;
+  view_t* cursor_v;
+
   textstyle_t style;
-  char        active; // we are under editing
+  char        active;
   void*       userdata;
 
   void (*on_text)(view_t* view);
@@ -46,9 +47,9 @@ void   vh_textinput_set_on_deactivate(view_t* view, void (*event)(view_t*));
 
 void vh_textinput_upd(view_t* view)
 {
-  vh_textinput_t* data  = view->handler_data;
-  str_t*          text  = data->text;
-  r2_t            frame = view->frame.local;
+  vh_textinput_t* data   = view->handler_data;
+  str_t*          text_s = data->text_s;
+  r2_t            frame  = view->frame.local;
 
   if (data->active)
   {
@@ -57,18 +58,18 @@ void vh_textinput_upd(view_t* view)
     // update cursor
   }
 
-  char empty = text->length == 0;
-  if (empty) str_addbytearray(text, " ");
+  char empty = text_s->length == 0;
+  if (empty) str_addbytearray(text_s, " ");
 
-  glyph_t* glyphs = malloc(sizeof(glyph_t) * text->length);
-  for (int i = 0; i < text->length; i++) glyphs[i].cp = text->codepoints[i];
+  glyph_t* glyphs = malloc(sizeof(glyph_t) * text_s->length);
+  for (int i = 0; i < text_s->length; i++) glyphs[i].cp = text_s->codepoints[i];
 
-  text_layout(glyphs, text->length, data->style, frame.w, frame.h);
+  text_layout(glyphs, text_s->length, data->style, frame.w, frame.h);
 
   // update cursor position
 
   glyph_t last = {0};
-  if (text->length > 0) last = glyphs[text->length - 1];
+  if (text_s->length > 0) last = glyphs[text_s->length - 1];
 
   r2_t crsr_f = {0};
   crsr_f.x    = last.x + last.w + 1;
@@ -76,23 +77,23 @@ void vh_textinput_upd(view_t* view)
   crsr_f.w    = 2;
   crsr_f.h    = last.asc;
 
-  vh_anim_set(data->cursor, data->cursor->frame.local, crsr_f, 10, AT_LINEAR);
+  vh_anim_set(data->cursor_v, data->cursor_v->frame.local, crsr_f, 10, AT_LINEAR);
 
-  // view_set_frame(data->cursor, crsr_f);
+  // view_set_frame(data->cursor_v, crsr_f);
 
   free(glyphs);
 
-  //textinput_render_glyphs(glyphs, text->length, style, bitmap);
+  //textinput_render_glyphs(glyphs, text_s->length, style, bitmap);
   //vh_anim_add(glyphview);
   //vh_anim_set(glyphview, sf, ef, 10, AT_LINEAR);
 
-  char* cstr = str_cstring(text);
+  char* cstr = str_cstring(text_s);
 
   tg_text_set(view, cstr, data->style);
 
   REL(cstr);
 
-  if (empty) str_removecodepointatindex(text, 0);
+  if (empty) str_removecodepointatindex(text_s, 0);
 }
 
 void vh_textinput_activate(view_t* view, char state)
@@ -104,7 +105,7 @@ void vh_textinput_activate(view_t* view, char state)
     if (!data->active)
     {
       data->active = 1;
-      view_add(view, data->cursor);
+      view_add(view, data->cursor_v);
     }
   }
   else
@@ -112,7 +113,7 @@ void vh_textinput_activate(view_t* view, char state)
     if (data->active)
     {
       data->active = 0;
-      view_remove(view, data->cursor);
+      view_remove(view, data->cursor_v);
     }
   }
 
@@ -142,7 +143,7 @@ void vh_textinput_evt(view_t* view, ev_t ev)
   }
   else if (ev.type == EV_TEXT)
   {
-    str_addbytearray(data->text, ev.text);
+    str_addbytearray(data->text_s, ev.text);
 
     vh_textinput_upd(view);
 
@@ -150,9 +151,9 @@ void vh_textinput_evt(view_t* view, ev_t ev)
   }
   else if (ev.type == EV_KDOWN)
   {
-    if (ev.keycode == SDLK_BACKSPACE && data->text->length > 0)
+    if (ev.keycode == SDLK_BACKSPACE && data->text_s->length > 0)
     {
-      str_removecodepointatindex(data->text, data->text->length - 1);
+      str_removecodepointatindex(data->text_s, data->text_s->length - 1);
       vh_textinput_upd(view);
       if (data->on_text) (*data->on_text)(view);
     }
@@ -171,56 +172,59 @@ void vh_textinput_add(view_t*     view,
 {
   printf("vh_textinput_add %s %s\n", view->id, phtext);
 
+  char* id_c = cstr_fromformat("%s%s", view->id, "crsr", NULL);
+
   vh_textinput_t* data = mem_calloc(sizeof(vh_textinput_t), "vh_text", NULL, NULL);
-  data->text           = str_new();
-  data->style          = textstyle;
-  data->userdata       = userdata;
 
-  if (text) str_addbytearray(data->text, text);
+  data->text_s   = str_new();
+  data->glyph_v  = VNEW();
+  data->cursor_v = view_new(id_c, (r2_t){50, 12, 2, 0});
 
-  view->needs_key    = 1;
-  view->needs_text   = 1;
+  data->style    = textstyle;
+  data->userdata = userdata;
+
+  view->needs_key  = 1; // backspace event
+  view->needs_text = 1; // unicode text event
+
   view->handler      = vh_textinput_evt;
   view->handler_data = data;
 
-  // add default/first paragraph
+  // cursor
 
-  r2_t frame = view->frame.local;
-  frame.x    = 0;
-  frame.y    = 0;
+  data->cursor_v->layout.background_color = 0x666666FF;
+  tg_css_add(data->cursor_v);
+  vh_anim_add(data->cursor_v);
+
+  // view setup
 
   tg_text_add(view);
 
-  // add cursor
+  // text_s setup
 
-  char*   crsrid = cstr_fromformat("%s%s", view->id, "crsr", NULL);
-  view_t* cursor = view_new(crsrid, (r2_t){50, 12, 2, 0});
-  REL(crsrid);
+  if (text) str_addbytearray(data->text_s, text);
 
-  cursor->layout.background_color = 0x666666FF;
-  tg_css_add(cursor);
-  vh_anim_add(cursor);
-
-  data->cursor = cursor;
-
-  // udpate
+  // update text
 
   vh_textinput_upd(view);
+
+  // cleanup
+
+  REL(id_c);
 }
 
 void vh_textinput_set_text(view_t* view, char* text)
 {
   vh_textinput_t* data = view->handler_data;
 
-  str_reset(data->text);
-  str_addbytearray(data->text, text);
+  str_reset(data->text_s);
+  str_addbytearray(data->text_s, text);
   vh_textinput_upd(view);
 }
 
 str_t* vh_textinput_get_text(view_t* view)
 {
   vh_textinput_t* data = view->handler_data;
-  return data->text;
+  return data->text_s;
 }
 
 void vh_textinput_set_on_text(view_t* view, void (*event)(view_t*))
